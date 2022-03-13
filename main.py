0b1573ddc6d7dae3983e4e880ad7619055d5d0c9
andreanaldini <naldini.andrea@gmail.com>
2022-03-13 16:26:03 +0100

    Add retrieve column func

--- main.py
+++ main.py
@@ -2,15 +2,39 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#imports
+import platform
+import sqlite_creator
+import os
+#global variables
+system = platform.system()
+PATH = os.getcwd()
+#Manage different OS
+if system == 'Windows':
+    separatore = '\\'
+    pass
+else:
+    separatore = '/'
+    pass
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+    print('Hi, {name}'.format(name=name))  # Press Ctrl+F8 to toggle the breakpoint.
+    return
 # Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    print_hi('PyCharm run on {system} OS'.format(system=system))
+    #create new db
+    FALAB_DB = sqlite_creator.DB_sqlite(PATH,'FALAB_db.db',separatore)
+    FALAB_DB.create_table('Analisi')
+    FALAB_DB.add_col('Analisi','Care','INT','NOT NULL DEFAULT 1')
+    col = FALAB_DB.retrieve_col('Analisi')
+    print(col)
+    FALAB_DB.add_col('Analisi','Note','CHAR')
+    col = FALAB_DB.retrieve_col('Analisi')
+    print(col)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- sqlite_creator.py
+++ sqlite_creator.py
@@ -5,15 +5,16 @@ This file is a general purpose sqplite db manipulation
 import sqlite3
 
 class DB_sqlite():
-    def __init__(self,path_db, db_name):
+    def __init__(self,path_db, db_name, separatore):
         '''
 
         :param path_db: path where db will be created type STRING
         :param db_name: name of database type STRING
         '''
         self.path = path_db
-        self.name = db_name
-        db = sqlite3.connect('{path}\\{db}'.format(path=self.path,db = self.db_name))
+        self.db_name = db_name
+        self.separatore = separatore
+        db = sqlite3.connect('{path}{separatore}{db}'.format(path=self.path,separatore=self.separatore,db = self.db_name))
         db.close()
         return
 
@@ -25,17 +26,18 @@ class DB_sqlite():
         :return:
         '''
         # Connessione al Database
-        db = sqlite3.connect('{path}\\{db}'.format(path=self.path,db = self.db_name))
+        db = sqlite3.connect('{path}{separatore}{db}'.format(path=self.path,separatore=self.separatore,db = self.db_name))
         #creazione del cursore
-        cursor_db = self.db.cursor()
+        cursor_db = db.cursor()
         #esecuzione comando
-        cursor_db.execute("CREATE TABLE IF NOT EXISTS {name_tab} (id int NOT NULL PRIMARY KEY AUTO_INCREMENT)".format(
+        cursor_db.execute("CREATE TABLE IF NOT EXISTS {name_tab} (id int AUTO_INCREMENT,PRIMARY KEY (id))".format(
             name_tab=table_name))
+        db.commit()
         # Disconnessione dal DB
         db.close()
         return
 
-    def add_col(self,table,name,type,spec):
+    def add_col(self,table,name,type,spec=None):
         '''
 
         :param name: Nome colonna
@@ -46,59 +48,74 @@ class DB_sqlite():
         '''
 
         # Connessione al Database
-        db = sqlite3.connect('{path}\\{db}'.format(path=self.path,db = self.db_name))
+        db = sqlite3.connect('{path}{separatore}{db}'.format(path=self.path,separatore=self.separatore,db = self.db_name))
         #creazione del cursore
-        cursor_db = self.db.cursor()
+        cursor_db = db.cursor()
         #esecuzione comando
-        cursor_db.execute("ALTER TABLE IF EXISTS {table} ADD {col_name} {data_type} {spec}".format(table=table,
+        cursor_db.execute("ALTER TABLE {table} ADD {col_name} {data_type} {spec}".format(table=table,
         col_name=name, data_type=type, spec=spec))
+        db.commit()
         # Disconnessione dal DB
         db.close()
         result = 'Column' + name + ' added'
         return result
 
+    def retrieve_col(self, table):
+        '''
+
+        :param table: Nome Tabella
+        :return: Ritorna la lista delle colonne della tabella
+        '''
+        db = sqlite3.connect('{path}{separatore}{db}'.format(path=self.path,separatore=self.separatore,db = self.db_name))
+        #creazione del cursore
+        cursor_db = db.cursor()
+        #Sanity check
+        cursor_db.execute("SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = {table}".format(table=table))
+        columns = cursor_db.fetchall()
+        # Disconnessione dal DB
+        db.close()
+
+        return columns
+
     def insert_record(self,table,record):
         '''
 
         :param table:
-        :param record:
+        :param record: Dictonary based on column name
         :return:
         '''
-        string_value = str()
-        for i in record:
-            string_value += i
-            string_value +=','
-            pass
+
         # Connessione al Database
-        db = sqlite3.connect('{path}\\{db}'.format(path=self.path,db = self.db_name))
+        db = sqlite3.connect('{path}{separatore}{db}'.format(path=self.path,separatore=self.separatore,db = self.db_name))
         #creazione del cursore
-        cursor_db = self.db.cursor()
-        #crea
-        #esecuzione comando
-        cursor_db.execute("ALTER TABLE IF EXISTS {table} ADD {col_name} {data_type} {spec}".format(table=table,
-        col_name=name, data_type=type, spec=spec))
+        cursor_db = db.cursor()
+        #Sanity check
+        cursor_db.execute("SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = {table}".format(table=table))
+        columns = cursor_db.fetchall()
+        #esecuzione comando inserimento valore
+        #cursor_db.execute("")
         # Disconnessione dal DB
         db.close()
-        result =  + name + ' added'
         return
 
     def change_record(self,table,loc,record):
         '''
-        La funzione modifica un record già esistente nella tabella selezionata
+        La funzione modifica un record esistente nella tabella selezionata
         :param table:
         :param loc:
         :param record:
         :return:
         '''
         # Connessione al Database
-        db = sqlite3.connect('{path}\\{db}'.format(path=self.path,db = self.db_name))
+        db = sqlite3.connect('{path}{separatore}{db}'.format(path=self.path,separatore=self.separatore,db = self.db_name))
         #creazione del cursore
-        cursor_db = self.db.cursor()
+        cursor_db = db.cursor()
         #esecuzione comando
-        cursor_db.execute("INSERT INTO {table} VALUES ({values})".format(table = table,values=string_value))
+        cursor_db.execute("INSERT INTO {table} VALUES ({values})".format(table = table,values='string_value'))
+        db.commit()
         # Disconnessione dal DB
         db.close()
-        result = 'Column' + string_value + ' added'
+        result = 'Column'
         return result
 
     def delete_record(self,table,loc,record):
@@ -110,14 +127,13 @@ class DB_sqlite():
         '''
 
         # Connessione al Database
-        db = sqlite3.connect('{path}\\{db}'.format(path=self.path,db = self.db_name))
+        db = sqlite3.connect('{path}{separatore}{db}'.format(path=self.path,separatore=self.separatore,db = self.db_name))
         #creazione del cursore
         cursor_db = self.db.cursor()
         #esecuzione comando
-        cursor_db.execute("ALTER TABLE IF EXISTS {table} ADD {col_name} {data_type} {spec}".format(table=table,
-        col_name=name, data_type=type, spec=spec))
+        cursor_db.execute('Delete record statement')
+        db.commit()
         # Disconnessione dal DB
         db.close()
-        result = 'Column' + name + ' added'
-        return result
+        return
     pass
