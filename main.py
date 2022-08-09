c0448a83c9c466daefb364df88289c61f8034f54
ITANNAL <andrea.naldini@it.abb.com>
2022-08-09 10:31:47 +0200

    Add New_user management. Add dialog box for register results.

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from kivymd.app import MDApp
 from kivymd.uix.toolbar import MDToolbar
 from kivymd.uix.list import MDList
 from kivymd.uix.list import OneLineIconListItem
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
 
 
 # global variables
@@ -45,6 +47,7 @@ class SQL_DatabaseApp(MDApp):
         Builder.load_file("Logged_in_interface.kv")
         Builder.load_file("Log_in_screen.kv")
         Builder.load_file("NavDrawerContent.kv")
+        self.dialog = User_dialog(title = 'User already registered',content_cls = Box_Contenent())
         return UserInterface()
 
     def Log_in(self, username, password):
@@ -78,6 +81,35 @@ class SQL_DatabaseApp(MDApp):
         Aggiunge il nuovo utente al database
         :return:
         '''
+        #check the error status of labels
+        err = bool()
+        user_dict = {'Nome': nome,'Cognome':cognome,'Username':username,'Password':password,'Email':email, 'Privilegi':privilegi}
+        for i in user_dict.keys():
+            if user_dict[i] == '':
+                err = False
+                break
+                pass
+            else:
+                err = True
+                pass
+            pass
+
+        if err:
+            l = SQL_DB.search_loc('Utenti',"Username",username)
+            if l:
+                self.dialog.title = 'User already registered'
+                self.dialog.open()
+                pass
+            else:
+                SQL_DB.insert_record('Utenti',user_dict)
+                self.dialog.title = 'User Added'
+                self.dialog.open()
+                pass
+            pass
+        else:
+            self.dialog.title = 'Missing Elements'
+            self.dialog.open()
+            pass
         return
 
     def set_screen(self, next_scr):
@@ -85,6 +117,24 @@ class SQL_DatabaseApp(MDApp):
         self.prev_screen = self.root.ids.root_screen.current
         self.root.ids.root_screen.current = next_scr
         print("old:{x} and new: {y}".format(x = self.prev_screen,y=next_scr))
+        self.root.ids.nav_drw.set_state("toggle")
+        return
+
+    def mng_dialog(self,title):
+        if title == 'User Added':
+
+            self.root.ids.root_screen.current= 'Logged_in_interface'
+            self.dialog.dismiss(force=True)
+            pass
+        elif title == 'User already registered':
+
+            self.dialog.dismiss(force=True)
+
+            pass
+        elif title == 'Missing Elements':
+            self.dialog.dismiss(force=True)
+        else:
+            pass
         return
     pass
 
@@ -108,7 +158,10 @@ class BaseTitleBar(MDToolbar):
         if current_screen == "new_user":
             self.right_action_items= []
             self.right_action_items = [["menu", lambda x: self.nav_drw.set_state("toggle")]]
+            self.root_screen.get_screen('new_user').clear_fields()
+            self.nav_drw.set_state("toggle")
             self.md_list.set_items(MDApp.get_running_app().privilege)
+
             pass
         elif current_screen == 'Logged_in_interface':
             self.right_action_items = []
@@ -123,6 +176,30 @@ class BaseTitleBar(MDToolbar):
 
 
 class New_user(Screen):
+
+    def round_active(self):
+        '''
+        The function scan the check box buttons and return the selected string
+        :return:
+        '''
+        if self.ids.quality_check_btn.active:
+            credential = 'Quality'
+            pass
+        elif self.ids.production_check_btn.active:
+            credential = 'Production'
+            pass
+        else:
+            credential = ''
+            pass
+        return credential
+
+    def clear_fields(self):
+        self.ids.Nome_input.text = ''
+        self.ids.Cognome_input.text = ''
+        self.ids.Username_input.text = ''
+        self.ids.e_mail.text = ''
+        self.ids.New_password_input.text = ''
+        return
     pass
 
 
@@ -175,7 +252,6 @@ class NavDrawerContent(MDList):
         The function will all the proper method for screen managing
         '''
 
-        print ("clicked icon {x}".format(x = icon_clicked))
         if icon_clicked == "logout": # Logout
             MDApp.get_running_app().set_screen("Log_in_Screen")
             pass
@@ -208,6 +284,12 @@ class NavDrawerContent(MDList):
 
 class List_item(OneLineIconListItem):
     icon = StringProperty()
+    pass
+
+class Box_Contenent(BoxLayout):
+    pass
+
+class User_dialog(MDDialog):
     pass
 
 if __name__ == '__main__':
@@ -261,16 +343,14 @@ if __name__ == '__main__':
     # Inserimento utente Base
     record_base = {'Nome':'Andrea','Cognome':'Naldini','Username':'Naldadoc','Email':'andrea.naldini@it.abb.com',
                    'Password':'Root','Privilegi':'admin'}
-    record_quality_test ={'Nome':'Quality','Cognome':'Process','Username':'Quality','Password':'Quality','Email':'Quality.test@it.abb.com','Privilegi':'Quality'}
-    record_repair_test = {'Nome':'Rework','Cognome':'Repair','Username':'Rework','Password':'Production','Email':'Quality.test@it.abb.com','Privilegi':'Production'}
-    l = SQL_DB.search_loc('Utenti', 'Nome', 'Admin')
+    l = SQL_DB.search_loc('Utenti', 'Nome', 'Andrea')
     if not l:
         SQL_DB.insert_record('Utenti', record_base)
-        SQL_DB.insert_record('Utenti', record_quality_test)
-        SQL_DB.insert_record('Utenti', record_repair_test)
         pass
     records = SQL_DB.show_records('Utenti')
-    print(records)
+    for i in records:
+        print(i)
+        pass
     SQL_DatabaseApp().run()
     pass
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
